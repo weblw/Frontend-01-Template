f4e52d56efb762b0b3ebe092bd97a2ef8ec5f2e9
weblw <872993425@qq.com>
2020-07-29 22:02:42 +0800

    week16

---
+++ week16/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+
+module.exports = {
+  entry: './main.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  mode: 'development',
+  optimization: {
+    minimize: false
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: [['@babel/plugin-transform-react-jsx',{
+              pragma:'create'
+            }]]
+          }
+        }
+      }
+    ]
+  }
+}
